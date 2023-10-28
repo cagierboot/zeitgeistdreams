dd289178b681752930f1be57f319a80e8fb04c05
Mark Ortega <markortega@Marks-MacBook-Pro-2.local>
2023-10-28 03:44:50 -0700

    front end exists

---
+++ back.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import openai
+
+import os
+print(os.path.abspath("templates"))
+
+
+app = Flask(__name__)
+
+openai.api_key = 'YOUR_API_KEY_HERE'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_input = request.form['user_input']
+    response = openai.ChatCompletion.create(
+        model="ft:gpt-3.5-turbo-0613:markortega::8EaZbTou",
+        messages=[
+            {"role": "system", "content": "This assistant is Mark Ortega. They share the same opinions and approaches to ideas about the world"},
+            {"role": "user", "content": user_input}
+        ]
+    )
+    return response.choices[0].message['content']
+
+if __name__ == '__main__':
+    app.run(debug=True)
