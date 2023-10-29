0a9304c161337113f3bad0e6464c10c7d98d31f3
Mark Ortega <markortega@Marks-MacBook-Pro-2.local>
2023-10-28 23:55:16 -0700

    extract 3 data sets

--- trendExtract.py
+++ trendExtract.py
@@ -10,27 +10,26 @@ user_agent = 'app:v1.0 cagierboot)'
 # Setup Reddit Client
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
 
-# Specify the subreddit
-subreddit_name = "news"  # Example subreddit
+# Subreddits to fetch data from
+subreddits = ["news", "Art", "science"]
 
-# Fetching top 10 hot posts
-top_posts = reddit.subreddit(subreddit_name).hot(limit=10)
+# Store all posts from different categories
+all_posts_data = []
 
-# Storing posts in a list
-posts_data = []
+for subreddit_name in subreddits:
+    top_posts = reddit.subreddit(subreddit_name).hot(limit=5)
+    
+    for post in top_posts:
+        created_date = datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d')
 
-for post in top_posts:
-    # Convert the creation time from Unix timestamp to readable format
-    created_date = datetime.utcfromtimestamp(post.created_utc).strftime('%Y-%m-%d')
-
-    post_info = {
-        'prompt': f"This was trending on {created_date}:",
-        'completion': f"\n{post.title}\n"
-    }
-    posts_data.append(post_info)
+        post_info = {
+            'prompt': f"This was trending on {created_date}:",
+            'completion': f"\n{post.title}\n"
+        }
+        all_posts_data.append(post_info)
 
 # Writing data to a JSON file
 with open('top_posts.json', 'w', encoding='utf-8') as f:
-    json.dump(posts_data, f, ensure_ascii=False, indent=4)
+    json.dump(all_posts_data, f, ensure_ascii=False, indent=4)
 
 print("Top posts have been saved to 'top_posts.json'")
