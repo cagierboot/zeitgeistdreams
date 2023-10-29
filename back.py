6820073337a35109a737f9d145f14fc172f929cf
Mark Ortega <markortega@Marks-MacBook-Pro-2.local>
2023-10-29 04:49:09 -0700

    generate button works

--- back.py
+++ back.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template
 import openai
 
 app = Flask(__name__)
-openai.api_key = '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'
+openai.api_key = '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'
 
 # Global variable to store conversation - not recommended for production
 conversation = []
@@ -27,11 +27,33 @@ def get_response():
         ]
     )
 
-
     ai_response = response.choices[0].message['content']
     conversation.append(f"Arachne: {ai_response}")  # Add AI response to conversation
 
     return render_template('index.html', conversation=conversation)
+
+@app.route('/special_query', methods=['POST'])
+def special_query():
+    global conversation
+    data = request.get_json()
+    user_input = data['user_input']
+    conversation.append(f"You (special): {user_input}")
+
+    special_response = openai.ChatCompletion.create(
+        model="ft:gpt-3.5-turbo-0613:markortega::8EvlX0TG",
+        messages=[
+            {"role": "system", "content": "Please generate a poem based on your dream"},
+            {"role": "user", "content": user_input}
+        ]
+    )
+
+    ai_response = special_response.choices[0].message['content']
+    conversation.append(f"Arachne (Special): {ai_response}")
+
+    print(f"Special Query Response: {ai_response}")
+    
+    # Instead of rendering a template, return a JSON response
+    return jsonify({"message": "Special query processed", "response": ai_response})
 
 @app.route('/clear_conversation', methods=['POST'])
 def clear_conversation():
